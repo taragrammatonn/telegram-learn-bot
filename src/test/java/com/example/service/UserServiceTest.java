9264291390b59cdee855957b506a379b2c3b18f8
taragrammatonn <49160107+taragrammatonn@users.noreply.github.com>
2024-06-17 15:16:41 +0300

    Update UserServiceTest.java
    Fixed integration test

--- src/test/java/com/example/service/UserServiceTest.java
+++ src/test/java/com/example/service/UserServiceTest.java
@@ -1,68 +1,63 @@
 package com.example.service;
 
 import com.example.dao.UserDAO;
-import com.example.dao.model.User;
 import com.example.factory.UserFactory;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.web.client.RestClient;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class UserServiceTest {
-
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(
-      "postgres:15-alpine"
-    );
+class UserServiceTest {
 
     @Autowired
     private TelegramUserService telegramUserService;
 
-    @InjectMocks
+    @MockBean
     private UserDAO userDAO;
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
     private MockMvc mvc;
 
+    @BeforeEach
+    public void setup() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .build();
+    }
+
     @Test
-    public void shouldReturnUserById() throws Exception {
+    void shouldReturnUserById() throws Exception {
 
         var testUser = UserFactory.aUserWithId();
 
         var objectMapper = new ObjectMapper();
         var userJson = objectMapper.writeValueAsString(testUser);
 
-        when(userDAO.findById(any())).thenReturn(Optional.of(testUser));
+        when(userDAO.findById(testUser.getId())).thenReturn(Optional.of(testUser));
 
-        mvc.perform(get("/api/user/1")
-                .contentType(APPLICATION_JSON))
+        mvc.perform(get("/api/user/1"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().json(userJson));
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(content().json(userJson, true));
     }
 }
