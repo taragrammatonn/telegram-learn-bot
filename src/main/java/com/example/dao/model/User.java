b2852553bd0ef6eded8153e81aa8eb31e6bfebb4
Kostreic <kostrovalexandru@gmail.com>
2024-07-04 19:27:34 +0300

    Removed the import

--- src/main/java/com/example/dao/model/User.java
+++ src/main/java/com/example/dao/model/User.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import java.util.UUID;
-
 @Entity(name = "User")
 @Table(schema = "users", name = "User")
 public class User {
